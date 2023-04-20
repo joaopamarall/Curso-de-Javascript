27675646efb8ab0b88e44e6b0910fde1aec03689
joaopamarall <joaoamaral56@hotmail.com>
2023-04-20 20:56:32 -0300

    commitando estudos

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'uesclei']
+const todos = filhas.concat(filhos, 'Yerleyano')
+console.log(todos, filhas, filhos)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
